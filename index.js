7f69ef75fa769d4b5d8a73a5f7bca29b116e38f3
ummeyoni <ummeyoni1995@gmail.com>
2021-05-10 21:06:59 +0600

    again added

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ require("dotenv").config();
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rkooy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("doctors"));
@@ -24,11 +23,13 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
 client.connect(err => {
-  const appointmentCollection = client
-    .db("doctorsPortal")
-    .collection("appointments");
+  console.log(err);
+  
+  const appointmentCollection = client.db("doctorsPortal").collection("appointments");
   const doctorCollection = client.db("doctorsPortal").collection("doctors");
+  const customerCollection = client.db("doctorsPortal").collection("contact");
 
   app.post("/addAppointment", (req, res) => {
     const appointment = req.body;
@@ -37,14 +38,7 @@ client.connect(err => {
     });
   });
 
-  // app.get("/bookAppointment", (req, res) => {
-  //   appointmentCollection.find({}).toArray((err, documents) => {
-  //     res.send(documents);
-  //   });
-  // });
-
   app.get("/appointments", (req, res) => {
-    // console.log(req.query.service);
     appointmentCollection
       .find({ service: req.query.service })
       .toArray((err, documents) => {
@@ -52,10 +46,17 @@ client.connect(err => {
       });
   });
 
+  
+  app.get('/allAppointments', (req, res) => {
+    appointmentCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
   app.post("/appointmentsByDate", (req, res) => {
     const date = req.body;
     console.log(date);
-
     appointmentCollection
       .find({ date: date.date })
       .toArray((err, documents) => {
@@ -63,13 +64,15 @@ client.connect(err => {
       });
   });
 
+ 
+
   app.post("/addADoctor", (req, res) => {
     const file = req.files.file;
     const name = req.body.name;
     const email = req.body.email;
     const newImg = file.data;
     const encImg = newImg.toString("base64");
-
+    console.log(name);
     var image = {
       contentType: file.mimetype,
       size: file.size,
@@ -93,6 +96,15 @@ client.connect(err => {
       res.send(doctors.length > 0);
     });
   });
+
+  app.post("/contact", (req, res) => {
+    const customerInfo = req.body;
+    customerCollection.insertOne(customerInfo)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
 });
 
 app.listen(process.env.PORT || port);
